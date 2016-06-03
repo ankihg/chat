16e176a5f37319c6a96308b75918528a88c790a8
ankihg <hagelin.annika@gmail.com>
2016-06-02 19:01:22 -0700

    socket connection

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const config = require('./config');
 const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/src/index.html');
 })
 
-app.listen(config.PORT, () => {
+io.on('connection', (socket) => {
+  console.log('welcome user');
+});
+
+http.listen(config.PORT, () => {
   console.log(`server speaking on port ${config.PORT}`);
 })
